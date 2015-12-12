6e8e464b5f858ddd8cfb3fb57dfad59ffe29a690
Edwin Villanueva <edwincv0@gmail.com>
2015-12-11 18:37:23 -0800

    Commit for Heroku. Reviewed By; Edwin Villanueva

---
+++ server/mailer.js
@@ -0,0 +1,36 @@
+var nodemailer = Npm.require('nodemailer');
+
+Meteor.startup(function(){
+    console.log('Starting the Meteor Up.');
+});
+
+var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: 'dinebellavitawebmailer@gmail.com',
+        pass: 'BellaVita1'
+    }
+});
+
+
+
+Router.route('/contact', {where: 'server'}).post(function(){
+    var request = this.request.body;
+    var to = request.to, subject = request.subject, body = request.body;
+    sendMail(to, subject, body);
+    var json = {"result":"success"}; // what ever data you want to return
+    this.response.setHeader('Content-Type', 'application/json');
+    this.response.end(JSON.stringify(json));
+});
+
+function sendMail(to, subject, body){
+    transporter.sendMail({
+        from: 'mailer@dinebellavita.com',
+        to: to,
+        subject: subject,
+        text: body
+    });
+}
+
+
+
