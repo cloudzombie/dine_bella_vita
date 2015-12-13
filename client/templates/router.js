e2d55b0f1c40db0a0218118dcbf4a89bad239790
Edwin Villanueva <edwincv0@gmail.com>
2015-12-13 13:49:41 -0800

    Started implementation of angular into code. Reviewed By: Edwin Villanueva

--- client/templates/router.js
+++ client/templates/router.js
@@ -1,30 +1,30 @@
-Router.route('/', function () {
-    this.render('home', {
-    });
-});
-
-Router.route('/restaurant', function () {
-    this.render('restaurant', {
-    });
-});
-
-Router.route('/privatedining', function () {
-    this.render('restaurant', {
-    });
-});
-
-Router.route('/events', function () {
-    this.render('restaurant', {
-    });
-});
-
-Router.route('/contact', function () {
-    this.render('contact', {
-    });
-});
-
-Router.route('/thankyou', function () {
-    this.render('thank_you', {
-    });
-});
-
+//Router.route('/', function () {
+//    this.render('home', {
+//    });
+//});
+//
+//Router.route('/restaurant', function () {
+//    this.render('restaurant', {
+//    });
+//});
+//
+//Router.route('/privatedining', function () {
+//    this.render('restaurant', {
+//    });
+//});
+//
+//Router.route('/events', function () {
+//    this.render('restaurant', {
+//    });
+//});
+//
+//Router.route('/contact', function () {
+//    this.render('contact', {
+//    });
+//});
+//
+//Router.route('/thankyou', function () {
+//    this.render('thank_you', {
+//    });
+//});
+//
