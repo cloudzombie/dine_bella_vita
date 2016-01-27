bdc743e11b19937c0d27aae5106c1f0ecc54e414
Edwin Villanueva <edwincv0@gmail.com>
2016-01-27 10:49:34 -0800

    Added Happy Hour Directive

--- client/angular/controllers/homeController.js
+++ client/angular/controllers/homeController.js
@@ -1,7 +1,15 @@
 dineBellaVitaApp.controller('HomeController', ['$scope', function($scope){
+    $scope.showme = true;
+
     console.log("Home Controller Loaded");
-}]).directive('myCarousel', function(){
+}])
+.directive('myCarousel', function(){
     return {
         templateUrl: 'client/ng-templates/directives/carousel.ng.html'
+    }
+})
+.directive('happyHour', function(){
+    return {
+        templateUrl: 'client/ng-templates/directives/happyhour.ng.html'
     }
 });
